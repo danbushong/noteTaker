61b1b7fc7035d3ec51c61117949618a05e2a2ffd
Dan Bushong <bushongdan@gmail.com>
2021-02-01 16:34:17 -0600

    stacko says utf8 works and it does, not 100 % on why

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
 
 app.get("/notes", function(req,res){
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "public/index.html"));
-})
 
-app.get("api/notes", function(req, res){
+app.get("/api/notes", function(req, res){
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
 app.post("/api/notes", function( req, res){
-    //might need to use that star thing
-    fs.readFile(path.join(__dirname, "db.json"), function(error, response){
+    //
+    fs.readFile(path.join(__dirname, "db.json"),"utf8", function(error, response){
         if(error){
             console.log(error);
         }
@@ -40,18 +40,18 @@ app.post("/api/notes", function( req, res){
             text: noteRequest.text
         };
         notes.push(newNote);
-        res.JSON(newNote);
-        //here too for the star thing
+        res.json(newNote);
+        //
         fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(err){
             if(err) throw err;
         });
     });
 });
 
-app.delete("/api/notes/:id", function(req, res){
+app.delete("/api/notes:id", function(req, res){
     const deleteId = req.params.id;
-    //star thing here too maybe
-    false.readFile("db.json", function(error, response){
+    //
+    false.readFile("db.json","utf8", function(error, response){
         if(error) {
             console.log(error);
         }
