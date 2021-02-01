749d439fe1b03ae9902040f98d7cbc0ec20a6095
Dan Bushong <bushongdan@gmail.com>
2021-02-01 14:50:53 -0600

    set up the server get, now time for post and delete

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+//set up express + heroku
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+//go into the public folder
+app.use(express.static("public"));
+
+
+
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+app.get("api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "db.json"));
+});
